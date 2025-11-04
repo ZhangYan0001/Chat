31a53f4ecabee1cabf436815100f47ece948b0c5
ZhangYan <82816266+ZhangYan0001@users.noreply.github.com>
2025-11-04 22:11:44 +0800

    fixed the httpmgr

--- head/httpmgr.h
+++ head/httpmgr.h
@@ -25,16 +25,19 @@ class HttpMgr : public QObject,
  public slots:
 
   void http_finish_slot(ReqId id, HttpResponse rep, Modules mod);
+  void on_http_reply_finished(QNetworkReply* reply);
 
  private:
   friend class Singleton<HttpMgr>;
   HttpMgr();
   QNetworkAccessManager _manager;
+  void HandleNetworkError(QNetworkReply* reply, HttpResponse& reps);
+  HttpResponse ParseResponse(QNetworkReply* reply);
  signals:
 
   void http_finish_signal(ReqId id, HttpResponse rep, Modules mod);
-  void register_finish_signal(ReqId id, HttpResponse rep, Modules mod);
-  void login_finish_signal(ReqId id, HttpResponse rep, Modules mod);
+  void register_finish_signal(ReqId id, HttpResponse rep, Modules mod);  //
+  void login_finish_signal(ReqId id, HttpResponse rep, Modules mod);     //
 };
 
 struct HttpResponse {
@@ -45,4 +48,24 @@ struct HttpResponse {
   ErrorCodes errorCode;
 };
 
+class ResponseHandler : public QObject, public Singleton<ResponseHandler> {
+  Q_OBJECT
+  friend class Singleton<ResponseHandler>;
+
+ public:
+  void InitConnections();
+
+ signals:
+  void login_response_signal(const HttpResponse& rep);
+  void register_response_signal(const HttpResponse& rep);
+
+ private slots:
+  void on_http_finish(ReqId id, HttpResponse rep, Modules mod);
+
+ private:
+  ResponseHandler();
+  ~ResponseHandler();
+  void login_response_handler(const HttpResponse& rep);
+  void register_response_handler(const HttpResponse& rep);
+};
 #endif  // HTTPMGR_H
--- src/httpmgr.cpp
+++ src/httpmgr.cpp
@@ -6,113 +6,81 @@ HttpMgr::HttpMgr() {
 
 HttpMgr::~HttpMgr() {}
 
-// void HttpMgr::PostHttpReq(QUrl url, QJsonObject json, ReqId req_id,
-//                           Modules mod) {
-//   QByteArray data = QJsonDocument(json).toJson();
 
-//   QNetworkRequest request(url);
-//   request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-//   request.setHeader(QNetworkRequest::ContentLengthHeader,
-//                     QByteArray::number(data.length()));
-
-//   auto self = shared_from_this();
-//   QNetworkReply *reply = _manager.post(request, data);
-
-//   QObject::connect(
-//       reply, &QNetworkReply::finished, [reply, self, req_id, mod]() {
-//         int statusCode =
-//         reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
-//         qDebug() << "the status code is >> ---" << statusCode << "---<<";
-//         QString res = reply->readAll();
-//         if (reply->error() != QNetworkReply::NoError) {
-//           qDebug() << "reply error>>---"<< reply->errorString()<<"---<<";
-//           emit self->http_finish_signal(req_id, "",
-//           ErrorCodes::ERROR_NETWORK,
-//                                         mod);
-//           reply->deleteLater();
-//           return;
-//         }
-
-//         if (statusCode !=200) {
-//           qDebug() << "HTTP error code:" << statusCode;
-//           emit self->http_finish_signal(req_id, res, ErrorCodes::ERROR_HTTP,
-//           mod); reply->deleteLater(); return;
-//         }
-//         qDebug() << "reply is>>---"<<res.toStdString() << "---<<";
-//         emit self->http_finish_signal(req_id, res, ErrorCodes::SUCCESS, mod);
-//         reply->deleteLater();
-//         return;
-//       });
-// }
 void HttpMgr::PostHttpReq(QUrl url, QJsonObject json, ReqId req_id,
                           Modules mod) {
   QByteArray data = QJsonDocument(json).toJson();
 
   QNetworkRequest request(url);
+  QByteArray body = QJsonDocument(json).toJson(QJsonDocument::Compact);
   request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
   request.setHeader(QNetworkRequest::ContentLengthHeader,
                     QByteArray::number(data.length()));
 
-  auto self = shared_from_this();
+  // auto self = shared_from_this();
   QNetworkReply* reply = _manager.post(request, data);
+  reply->setProperty("req_id", static_cast<int>(req_id));
+  reply->setProperty("module", static_cast<int>(mod));
+  qDebug() << "[HttpMgr] POST:" << url.toString();
+  qDebug() << "[HttpMgr] Body:" << QString::fromUtf8(body);
 
-  QObject::connect(
-      reply, &QNetworkReply::finished, [reply, self, req_id, mod]() {
-        HttpResponse response;
-        response.httpStatus =
-            reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+  // QObject::connect(
+  //     reply, &QNetworkReply::finished, [reply, self, req_id, mod]() {
+  //       HttpResponse response;
+  //       response.httpStatus =
+  //           reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
 
-        QByteArray rawData = reply->readAll();
-        response.errorCode = ErrorCodes::SUCCESS;
+  //       QByteArray rawData = reply->readAll();
+  //       response.errorCode = ErrorCodes::SUCCESS;
 
-        // 1️⃣ 网络层错误
-        if (reply->error() != QNetworkReply::NoError) {
-          qWarning() << "Network error:" << reply->errorString();
-          response.errorCode = ErrorCodes::ERROR_NETWORK;
-          emit self->http_finish_signal(req_id, response, mod);
-          reply->deleteLater();
-          return;
-        }
+  //       // 1️⃣ 网络层错误
+  //       if (reply->error() != QNetworkReply::NoError) {
+  //         qWarning() << "Network error:" << reply->errorString();
+  //         response.errorCode = ErrorCodes::ERROR_NETWORK;
+  //         emit self->http_finish_signal(req_id, response, mod);
+  //         reply->deleteLater();
+  //         return;
+  //       }
 
-        // 2️⃣ HTTP 状态码错误
-        if (response.httpStatus != 200) {
-          qWarning() << "HTTP error:" << response.httpStatus;
-          response.errorCode = ErrorCodes::ERROR_HTTP;
-          emit self->http_finish_signal(req_id, response, mod);
-          reply->deleteLater();
-          return;
-        }
+  //       // 2️⃣ HTTP 状态码错误
+  //       if (response.httpStatus != 200) {
+  //         qWarning() << "HTTP error:" << response.httpStatus;
+  //         response.errorCode = ErrorCodes::ERROR_HTTP;
+  //         emit self->http_finish_signal(req_id, response, mod);
+  //         reply->deleteLater();
+  //         return;
+  //       }
 
-        // 3️⃣ 解析 JSON
-        QJsonParseError parseError;
-        QJsonDocument doc = QJsonDocument::fromJson(rawData, &parseError);
-        if (parseError.error != QJsonParseError::NoError) {
-          qWarning() << "JSON parse error:" << parseError.errorString();
-          response.errorCode = ErrorCodes::ERROR_JSON_PARSE;
-          emit self->http_finish_signal(req_id, response, mod);
-          reply->deleteLater();
-          return;
-        }
+  //       // 3️⃣ 解析 JSON
+  //       QJsonParseError parseError;
+  //       QJsonDocument doc = QJsonDocument::fromJson(rawData, &parseError);
+  //       if (parseError.error != QJsonParseError::NoError) {
+  //         qWarning() << "JSON parse error:" << parseError.errorString();
+  //         response.errorCode = ErrorCodes::ERROR_JSON_PARSE;
+  //         emit self->http_finish_signal(req_id, response, mod);
+  //         reply->deleteLater();
+  //         return;
+  //       }
 
-        if (doc.isObject()) {
-          QJsonObject obj = doc.object();
-          response.code = obj.value("code").toInt(-1);
-          response.msg = obj.value("message").toString();
-          response.data = obj.value("data").toObject();
+  //       if (doc.isObject()) {
+  //         QJsonObject obj = doc.object();
+  //         response.code = obj.value("code").toInt(-1);
+  //         response.msg = obj.value("message").toString();
+  //         response.data = obj.value("data").toObject();
 
-          // 4️⃣ 后端业务码检查
-          if (response.code != 0) {
-            response.errorCode = ErrorCodes::ERROR_BACKEND;
-          }
-        } else {
-          response.errorCode = ErrorCodes::ERROR_JSON_FORMAT;
-        }
+  //         // 4️⃣ 后端业务码检查
+  //         if (response.code != 0) {
+  //           response.errorCode = ErrorCodes::ERROR_BACKEND;
+  //         }
+  //       } else {
+  //         response.errorCode = ErrorCodes::ERROR_JSON_FORMAT;
+  //       }
 
-        qDebug() << "HTTP Response:" << rawData;
+  //       qDebug() << "HTTP Response:" << rawData;
 
-        emit self->http_finish_signal(req_id, response, mod);
-        reply->deleteLater();
-      });
+  //       emit self->http_finish_signal(req_id, response, mod);
+  //       reply->deleteLater();
+  //     });
 }
 
 void HttpMgr::http_finish_slot(ReqId id, HttpResponse rep, Modules mod) {
@@ -122,4 +90,54 @@ void HttpMgr::http_finish_slot(ReqId id, HttpResponse rep, Modules mod) {
   if (mod == Modules::LOGIN) {
     emit login_finish_signal(id, rep, mod);
   }
+}
+
+void HttpMgr::on_http_reply_finished(QNetworkReply *reply) {
+  ReqId req_id = static_cast<ReqId>(reply->property("req_id").toInt());
+  Modules mod = static_cast<Modules>(reply->property("module").toInt());
+
+  HttpResponse reps;
+  if (reply->error() != QNetworkReply::NoError) {
+    HandleNetworkError(reply, reps);
+    emit http_finish_signal(req_id, reps, mod);
+  }
+}
+
+void HttpMgr::HandleNetworkError(QNetworkReply *reply, HttpResponse &reps){
+
+}
+
+ResponseHandler::ResponseHandler() {
+  InitConnections();
+}
+
+ResponseHandler::~ResponseHandler() {}
+
+void ResponseHandler::InitConnections() {
+  connect(HttpMgr::GetInstance().get(), &HttpMgr::http_finish_signal, this,
+          &ResponseHandler::on_http_finish);
+}
+
+void ResponseHandler::on_http_finish(ReqId id, HttpResponse rep, Modules mod) {
+  Q_UNUSED(mod);
+
+  switch (id) {
+    case ReqId::ID_LOGIN_USER:
+      login_response_handler(rep);
+      break;
+    case ReqId::ID_REG_USER:
+      register_response_handler(rep);
+      break;
+    default:
+      qDebug() << "[ResponseHandler] 未处理的 ReqId: " << static_cast<int>(id);
+      break;
+  }
+}
+
+void ResponseHandler::register_response_handler(const HttpResponse& rep) {
+  emit register_response_signal(rep);
+}
+
+void ResponseHandler::login_response_handler(const HttpResponse& rep) {
+  emit login_response_signal(rep);
 }
